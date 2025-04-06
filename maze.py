68dedbd4ccb200aa7632e8aed890ed43c0b49bc7
robinzclark777 <robinzclark@gmail.com>
2025-04-06 12:11:00 -0400

    level 8

--- maze.py
+++ maze.py
@@ -2,7 +2,7 @@ import time
 from cell import Cell
 
 class Maze():
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
         self.cells = []
         self.x1 = x1
         self.y1 = y1
@@ -12,6 +12,7 @@ class Maze():
         self.cell_size_y = cell_size_y
         self.win = win
         self.create_cells()
+        self.break_entrance_and_exit()
 
     def create_cells(self):
         for i in range(self.num_cols):
@@ -39,7 +40,13 @@ class Maze():
         self.win.redraw()
         time.sleep(0.05)
 
-
+    def break_entrance_and_exit(self):
+        self.cells[0][0].has_left_wall = False
+        self.draw_cell(0,0)
+        bottom_row = self.num_cols -1 
+        right_column = self.num_rows - 1
+        self.cells[bottom_row][right_column].has_bottom_wall = False
+        self.draw_cell(bottom_row, right_column) 
 
 
 
--- tests.py
+++ tests.py
@@ -6,8 +6,7 @@ class Tests(unittest.TestCase):
     def test_maze_create_cells(self):
         num_cols = 12
         num_rows = 10
-        win = Window(100,100)
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10,win)
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1.cells),
             num_cols,
@@ -16,6 +15,28 @@ class Tests(unittest.TestCase):
             len(m1.cells[0]),
             num_rows,
         )
+
+    def test_maze_create_cells_large(self):
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1.cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1.cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells_entrance_exit(self):
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertFalse(
+            m1.cells[0][0].has_left_wall) 
+        self.assertFalse(
+            m1.cells[num_cols - 1][num_rows - 1].has_bottom_wall) 
 
 if __name__ == "__main__":
     unittest.main()
